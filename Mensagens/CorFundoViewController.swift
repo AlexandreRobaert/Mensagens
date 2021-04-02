a73e8215984793def84a342b391f770eaf367986
Alexandre Robaert <robaert.developer@icloud.com>
2021-04-01 23:26:18 -0300

    Layout finalizado

---
+++ Mensagens/CorFundoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CorFundoViewController.swift
+//  Mensagens
+//
+//  Created by Alexandre Robaert on 01/04/21.
+//
+
+import UIKit
+
+class CorFundoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
